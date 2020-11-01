eebe7f9c66147caca1f434d5c754202cdaf1099e
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-11-01 20:21:49 +0700

    TL_TC_ Giao dien form trang chu va cac su kien button

--- QuanLyNhanSu/frmMain.cs
+++ QuanLyNhanSu/frmMain.cs
@@ -15,8 +15,156 @@ namespace QuanLyNhanSu
 {
     public partial class frmMain : RadRibbonForm
     {
-       
+        //#
+        public static frmMain me;
+        public QuanLyNhanSu.Class.sql sql;
+        //#
+        public frmMain()
+        {
+           
 
-        
+        }
+        private Form KiemTraTonTai(Type formType)
+        {
+           
+        }
+        private string ChuoiTruyCapForm(string ten)
+        {
+          
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+           
+
+
+        }
+        public string layngay()
+        {
+            
+        }
+
+        private void btnNhanVien_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnTyLeLuongThang_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnChamCong_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnChucVu_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnPhanQuyen_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnBCLuongThang_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnBCNhanSu_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnHDHetHan_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnDangXuat_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btnDoiMatKau_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnTonGiao_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnDanToc_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnTrinhDo_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnChuyenMon_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnNgoaiNgu_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnPhongBan_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnTo_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnLoaiHopDong_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnTaoNguoiDung_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnThongTin_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnDMHieuQua_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void radImageButtonElement1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radMenuItem1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+
     }
 }
