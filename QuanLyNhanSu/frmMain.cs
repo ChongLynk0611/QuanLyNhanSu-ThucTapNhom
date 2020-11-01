636aa52aecb082cf44f3bb1e4df06c03bf1744be
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-11-01 09:59:46 +0700

    TL_Khoi tao project

---
+++ QuanLyNhanSu/frmMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Telerik.WinControls;
+using Telerik.Data;
+
+
+namespace QuanLyNhanSu
+{
+    public partial class frmMain : RadRibbonForm
+    {
+       
+
+        
+    }
+}
