19e1e0791cc3060340de89f58d1045120077b736
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-11-02 09:28:24 +0700

    BT_ Tao form nhan vien

---
+++ QuanLyNhanSu/frmNhanVien.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using Telerik.Data;
+using Telerik.WinControls.UI;
+using QuanLyNhanSu.Class;
+using Telerik.WinControls;
+using Telerik.WinControls.UI.Localization;
+using QuanLyNhanSu.Class;
+
+
+namespace QuanLyNhanSu
+{
+    public partial class frmNhanvien : RadForm
+    {
+
+        public frmNhanvien()
+        {
+            InitializeComponent();          
+        }
+    }
+}
