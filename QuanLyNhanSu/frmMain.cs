2d5bade3f08bb5ccef84411cb7d2f44a6f1583a5
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-11-02 09:32:16 +0700

    TL_TC_ Tao frm_config  va cac method co ban cua frmain

--- QuanLyNhanSu/frmMain.cs
+++ QuanLyNhanSu/frmMain.cs
@@ -21,16 +21,33 @@ namespace QuanLyNhanSu
         //#
         public frmMain()
         {
-           
+            readFile c = new readFile();
+            c.readfile();
+            string strServerName = "";
+            string strDatabase = "";
+            if (c.strLine1.Contains("Server:") == true) strServerName = c.strLine1.Substring(7);
+            if (c.strLine2.Contains("Database:") == true) strDatabase = c.strLine2.Substring(9);
+
+            InitializeComponent();
+            sql = new sql(strServerName, strDatabase,
+                "U0", "123");
+            me = this;
 
         }
         private Form KiemTraTonTai(Type formType)
         {
-           
+            foreach (Form f in this.MdiChildren)
+            {
+                if (f.GetType() == formType)
+                    return f;
+            }
+            return null;
         }
         private string ChuoiTruyCapForm(string ten)
         {
-          
+            return "Select * from tbl_user_form U inner join " +
+                  "tbl_DMform F on U.IDform=F.ID where U.username='" + sql.GetUser() + "' and  F.tenform='" + ten + "' and U.rights=1";
+
         }
         private void btnThoat_Click(object sender, EventArgs e)
         {
@@ -40,14 +57,22 @@ namespace QuanLyNhanSu
 
         private void frmMain_Load(object sender, EventArgs e)
         {
+            frmConfig obj = new frmConfig();
+            obj.ShowDialog();
 
-           
 
 
         }
         public string layngay()
         {
-            
+            string sel = "select top 1 percent Start from Appointments order by Start desc";
+            SqlConnection con = frmMain.me.sql.connectSQL();
+            con.Open();
+            SqlCommand sel1 = new SqlCommand(sel, con);
+            object o = sel1.ExecuteScalar();
+            con.Close();
+            return o.ToString();
+
         }
 
         private void btnNhanVien_Click(object sender, EventArgs e)
