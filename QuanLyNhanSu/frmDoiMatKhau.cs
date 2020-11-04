52b7b285eeec99cf804775ce38aa55b8a94683f2
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-11-04 08:22:38 +0700

    DA_TaoGiaoDienFormDoiMK

---
+++ QuanLyNhanSu/frmDoiMatKhau.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Telerik.WinControls;
+using QuanLyNhanSu.Class;
+namespace QuanLyNhanSu
+{
+    public partial class frmDoiMatKhau : RadForm
+    {
+        public frmDoiMatKhau()
+        {
+            InitializeComponent();
+        }
+    }
+}
