6df6f5bbd50e97392a9bc321e54daafdedb3bfbc
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-11-05 10:09:26 +0700

    BT_ Tao form kiem tra hop dong het han

---
+++ QuanLyNhanSu/frmKiemTraHDHetHan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Telerik.Data;
+using Telerik.WinControls.UI;
+using QuanLyNhanSu.Class;
+using Telerik.WinControls;
+
+namespace QuanLyNhanSu
+{
+    public partial class frmKiemTraHDHetHan : RadForm
+    {
+
+        public frmKiemTraHDHetHan()
+        {
+            InitializeComponent();
+        }
+    }
+}
