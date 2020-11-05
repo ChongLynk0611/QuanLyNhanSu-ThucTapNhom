5369a5ca59f10fc94553852177f2143cf5a6783b
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-11-05 08:29:18 +0700

    DA_TaoGiaoDienFormUser

---
+++ QuanLyNhanSu/frmUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Telerik.WinControls;
+using Telerik.Data;
+using QuanLyNhanSu.Class;
+namespace QuanLyNhanSu
+{
+    public partial class frmUser : RadForm
+    {
+        public frmUser()
+        {            
+            InitializeComponent();
+        }
+    }    
+}
