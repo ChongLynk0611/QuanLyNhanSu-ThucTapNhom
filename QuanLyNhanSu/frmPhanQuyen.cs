a291f1035c2ef692ab187a2c8c0155f8d12dca18
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-11-07 09:44:32 +0700

    DA_TaoGiaoDienFormPhanQuyen

---
+++ QuanLyNhanSu/frmPhanQuyen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+using Telerik.Data;
+using QuanLyNhanSu.Class;
+
+namespace QuanLyNhanSu
+{
+    public partial class frmPhanQuyen : RadForm
+    {
+        public frmPhanQuyen()
+        {
+            InitializeComponent();
+        }
+    }
+}
